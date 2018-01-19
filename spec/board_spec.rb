ff661fb2efcb828ffa3a2998fff2c1542e726818
chrisbrickey <cbrickey1@gmail.com>
2018-01-19 15:32:28 -0800

    refactor test object using let

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,2 +1,17 @@
 require 'rspec'
 require 'board'
+
+describe Board do
+
+  let(:subject) { Board.new }
+
+  describe "Board#display" do
+
+    it "shows a 5x5 grid" do
+
+      expect(subject.display).to eq("")
+    end
+
+
+  end
+end
