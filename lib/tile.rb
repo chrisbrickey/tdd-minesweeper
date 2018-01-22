d90a937f614a370430b0705b07683a4ce7c258f5
chrisbrickey <cbrickey1@gmail.com>
2018-01-22 13:46:18 -0800

    add unicodes for symbols to use later

--- lib/tile.rb
+++ lib/tile.rb
@@ -9,7 +9,18 @@ class Tile
     @revealed = false
   end
 
-
-
-
 end # end of Tile class
+
+
+#HOW TO PRINT UNICODE (see Unicode Character Table)
+# checkmark = "\u2713"
+# puts checkmark.encode('utf-8')
+
+# box = "\u20DE"
+# puts box.encode('utf-8')
+
+# bomb = "\u1F4A3"
+# puts bomb.encode('utf-8')
+
+# black_flag = "\u2691"
+# puts black_flag.encode('utf-8')
