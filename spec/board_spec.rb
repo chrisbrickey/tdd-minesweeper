7f97f1163622436dfafdb90dcbdde6e745c84a9e
chrisbrickey <cbrickey1@gmail.com>
2018-01-22 12:21:59 -0800

    write test to display multi-dimensional board based on input

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,20 +13,16 @@ describe Board do
   describe "Board#display" do
 
     it "shows a 2D grid of tiles" do
-      expected_two_by_two = "X X\nX X\n"
+      expected_two_by_two = "X X \nX X \n"
       actual_two_by_two = subject.display()
       expect(actual_two_by_two).to eq(expected_two_by_two)
     end
 
-    # it "shows a multi-D grid of tiles" do
-    #   expected_three_by_three = """
-    #                             X X X\n
-    #                             X X X\n
-    #                             X X X\n
-    #                             """
-    #   actual_three_by_three = subject.display(3)
-    #   expect(actual_three_by_three).to eq(expected_three_by_three)
-    # end
+    it "shows a multi-D grid of tiles" do
+      expected_three_by_three = "X X X \nX X X \nX X X \n"
+      actual_three_by_three = subject.display(3)
+      expect(actual_three_by_three).to eq(expected_three_by_three)
+    end
 
   end
 end
