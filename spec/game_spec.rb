8e5f61c151dd1e4f98b701200a01dff28bf06206
chrisbrickey <cbrickey1@gmail.com>
2018-01-22 20:13:21 -0800

    draft skeleton of tests for Game

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require 'game'
+
+describe Game do
+
+  describe "Game.prompt_user" do
+
+    xit "prompts the user for size of board" do
+    end
+
+    xit "does not accept values outside of 3-20" do
+    end
+
+  end
+
+end # of Game
