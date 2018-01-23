aec0c018a50a187c88a8a1e453b2c90cf9789a58
chrisbrickey <cbrickey1@gmail.com>
2018-01-22 20:18:21 -0800

    add game play to initialize board

--- lib/game.rb
+++ lib/game.rb
@@ -2,16 +2,26 @@ require 'board'
 require 'tile'
 
 class Game
-  def self.hi
-    puts "I'm a minesweeper game!"
-  end
 
   def self.play
-    board = Board.new
+    size = prompt_user
+    board = Board.new(size)
     board.display
   end
+
+  def self.prompt_user
+    puts "What size of board do you want to play on? (type an integer between 3 and 100, inclusive)\n"
+    size = gets.chomp
+    while !size.to_i.between?(3, 100)
+      puts "Please type an integer between 3 and 100, inclusive.\n"
+      size = gets.chomp
+    end
+
+    size.to_i
+  end
+
 end
 
 if __FILE__ == $0
-  Game.hi
+  Game.play
 end
